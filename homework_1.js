9563328b04922184ac1dc9124a24a2f9be5f4c3e
Briana Tyson <brianatys@gmail.com>
2017-05-02 21:12:09 -0400

    initial homework attempt

---
+++ homework_1.js
@@ -0,0 +1,22 @@
+
+// #1 //
+function addNumbers (num1, num2) {
+  var result = num1 + num2;
+  return(result)
+}
+
+var sum = (874, 35)
+
+
+// #2 //
+var cities = ["Los Angeles", " New York City", "Chicago";]
+var name = cities[0]
+cities[0]
+cities[2]
+cities[1]
+
+
+// #3 //
+var registration = {firstName: "Brett", lastName: "Balco", Age: 30};
+alert(registration.firstName);
+console.log("Hello, " + registration.firstName);
