d876c6f2da01954d00d37bfb06520a5dd60c3230
Briana Tyson <brianatys@gmail.com>
2017-05-27 09:14:38 -0400

    resubmission homework1

--- homework_1.js
+++ homework_1.js
@@ -1,12 +1,22 @@
 
 // #1 //
+var x= 7840;
+var y= 983;
+
+console.log(x + y);
+
+/*
+
+Initially created the wrong answer for #1 - Your feedback was: I saw you added an addNumbers function but didn't call it, what you had var sum = (874, 35) isn't actually valid javascript (you forgot to call the function name there)
+despite it not being the correct answer to the question, does this fix that?
+
 function addNumbers (num1, num2) {
   var result = num1 + num2;
   return(result)
 }
 
-var sum = (874, 35)
-
+var addNumbers = (874, 35)
+*/
 
 // #2 //
 var cities = ["Los Angeles", " New York City", "Chicago";]
